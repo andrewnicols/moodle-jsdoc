7c2163e1d64f2456e96881af98023d5578fbe55d
Andrew Nicols <andrew@nicols.co.uk>
2014-03-26 11:25:27 +0800

    Weekly build of YUI Documentation for 26 at doc_26

---
+++ moodle/26/api.js
@@ -0,0 +1,226 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Bootstrap.Collapse",
+        "Bootstrap.Dropdown",
+        "M.assignfeedback_editpdf.annotationhighlight",
+        "M.assignfeedback_editpdf.annotationline",
+        "M.assignfeedback_editpdf.annotationoval",
+        "M.assignfeedback_editpdf.annotationpen",
+        "M.assignfeedback_editpdf.annotationrectangle",
+        "M.assignfeedback_editpdf.annotationstamp",
+        "M.assignfeedback_editpdf.colourpicker.dropdown",
+        "M.assignfeedback_editpdf.comment",
+        "M.assignfeedback_editpdf.drawable",
+        "M.assignfeedback_editpdf.edit",
+        "M.assignfeedback_editpdf.editor.Editor",
+        "M.assignfeedback_editpdf.editor.assignfeedback_editpdf",
+        "M.assignfeedback_editpdf.editor.commentmenu",
+        "M.assignfeedback_editpdf.editor.commentsearch",
+        "M.assignfeedback_editpdf.editor.editor",
+        "M.assignfeedback_editpdf.point",
+        "M.assignfeedback_editpdf.quickcomment",
+        "M.assignfeedback_editpdf.quickcommentlist",
+        "M.assignfeedback_editpdf.rect",
+        "M.assignfeedback_editpdf.stamppicker.dropdown",
+        "M.assignfeedback_editpdf.widget.dropdown.dropdown",
+        "M.block_navigation",
+        "M.block_navigation.ActionKey",
+        "M.block_navigation.Branch",
+        "M.block_navigation.Tree",
+        "M.core",
+        "M.core.LockScroll",
+        "M.core.actionmenu",
+        "M.core.actionmenu.ActionMenu",
+        "M.core.ajaxException",
+        "M.core.alert",
+        "M.core.blockdraganddrop",
+        "M.core.blockdraganddrop.BlockRegion",
+        "M.core.blockdraganddrop.LegacyManager",
+        "M.core.blockdraganddrop.Manager",
+        "M.core.confirm",
+        "M.core.core",
+        "M.core.dialogue",
+        "M.core.dock",
+        "M.core.dock.ActionKey",
+        "M.core.dock.Block",
+        "M.core.dock.Dock",
+        "M.core.dock.DockedItem",
+        "M.core.dock.Loader",
+        "M.core.dock.Panel",
+        "M.core.dock.TabHeightManager",
+        "M.core.exception",
+        "M.core.notification",
+        "M.core.tooltip",
+        "M.course.management.Category",
+        "M.course.management.Console",
+        "M.course.management.Course",
+        "M.course.management.DragDrop",
+        "M.course.management.Item",
+        "M.tool_capability.search.Search",
+        "M.tool_capability.search.tool_capability",
+        "Moodle.core_course.util",
+        "Moodle.core_course.util.cm",
+        "Moodle.core_course.util.section",
+        "Moodle.theme_bootstrapbase.bootstrap",
+        "TreeView",
+        "TreeView.Sortable",
+        "Y.BootstrapEngine",
+        "Y.Moodle.course.categoryexpander"
+    ],
+    "modules": [
+        "gallery-bootstrap-collapse",
+        "gallery-bootstrap-dropdown",
+        "gallery-bootstrap-engine",
+        "gallery-sm-treeview",
+        "gallery-sm-treeview-sortable",
+        "moodle-assignfeedback_editpdf-editor",
+        "moodle-block_navigation-navigation",
+        "moodle-core-actionmenu",
+        "moodle-core-blockdraganddrop",
+        "moodle-core-dock",
+        "moodle-core-lockscroll",
+        "moodle-core-notification",
+        "moodle-core-notification-ajaxexception",
+        "moodle-core-notification-alert",
+        "moodle-core-notification-confirm",
+        "moodle-core-notification-dialogue",
+        "moodle-core-notification-exception",
+        "moodle-core-tooltip",
+        "moodle-course-categoryexpander",
+        "moodle-course-management",
+        "moodle-course-util",
+        "moodle-course-util-cm",
+        "moodle-course-util-section",
+        "moodle-tool_capability-search",
+        "theme_bootstrapbase-bootstrap"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-bootstrap-collapse",
+            "name": "gallery-bootstrap-collapse",
+            "description": "A Plugin which provides collapsing/expanding behaviors on a Node with\ncompatible syntax and markup from Twitter's Bootstrap project."
+        },
+        {
+            "displayName": "gallery-bootstrap-dropdown",
+            "name": "gallery-bootstrap-dropdown",
+            "description": "A Plugin which provides dropdown behaviors for dropdown buttons and menu\ngroups. This utilizes the markup from the Twitter Bootstrap Project."
+        },
+        {
+            "displayName": "gallery-bootstrap-engine",
+            "name": "gallery-bootstrap-engine",
+            "description": "Bootstrap Engine for Plug and Play widgets. This class is meant to be used in\nconjuntion with the Injection Engine (gallery-bootstrap-engine). It facilitates the use of\nan iframe as a sandbox to execute certain tasks and/or a presention element."
+        },
+        {
+            "displayName": "gallery-sm-treeview",
+            "name": "gallery-sm-treeview",
+            "description": "Provides the `Y.TreeView` widget."
+        },
+        {
+            "displayName": "gallery-sm-treeview-sortable",
+            "name": "gallery-sm-treeview-sortable",
+            "description": "Provides `Y.TreeView.Sortable`, a `Y.TreeView` extension that mixes in\n`Y.Tree.Sortable` and provides related TreeView-specific functionality."
+        },
+        {
+            "displayName": "moodle-assignfeedback_editpdf-editor",
+            "name": "moodle-assignfeedback_editpdf-editor",
+            "description": "Class representing a list of annotations."
+        },
+        {
+            "displayName": "moodle-block_navigation-navigation",
+            "name": "moodle-block_navigation-navigation",
+            "description": "Navigation block JS.\n\nThis file contains the Navigation block JS.."
+        },
+        {
+            "displayName": "moodle-core-actionmenu",
+            "name": "moodle-core-actionmenu",
+            "description": "Provides drop down menus for list of action links."
+        },
+        {
+            "displayName": "moodle-core-blockdraganddrop",
+            "name": "moodle-core-blockdraganddrop",
+            "description": "This file contains the Block Region class used by the drag and drop manager.\n\nProvides drag and drop functionality for blocks."
+        },
+        {
+            "displayName": "moodle-core-dock",
+            "name": "moodle-core-dock",
+            "description": "Dock JS.\n\nThis file contains the action key event definition that is used for accessibility handling within the Dock."
+        },
+        {
+            "displayName": "moodle-core-lockscroll",
+            "name": "moodle-core-lockscroll",
+            "description": "Provides the ability to lock the scroll for a page."
+        },
+        {
+            "displayName": "moodle-core-notification",
+            "name": "moodle-core-notification",
+            "description": "The notification module provides a standard set of dialogues for use\nwithin Moodle."
+        },
+        {
+            "displayName": "moodle-core-notification-ajaxexception",
+            "name": "moodle-core-notification-ajaxexception",
+            "description": "A dialogue type designed to display an appropriate error when an error\nthrown in the Moodle codebase was reported during an AJAX request."
+        },
+        {
+            "displayName": "moodle-core-notification-alert",
+            "name": "moodle-core-notification-alert",
+            "description": "A dialogue type designed to display an alert to the user."
+        },
+        {
+            "displayName": "moodle-core-notification-confirm",
+            "name": "moodle-core-notification-confirm",
+            "description": "A dialogue type designed to display a confirmation to the user."
+        },
+        {
+            "displayName": "moodle-core-notification-dialogue",
+            "name": "moodle-core-notification-dialogue",
+            "description": "The generic dialogue class for use in Moodle."
+        },
+        {
+            "displayName": "moodle-core-notification-exception",
+            "name": "moodle-core-notification-exception",
+            "description": "A dialogue type designed to display an appropriate error when a generic\njavascript error was thrown and caught."
+        },
+        {
+            "displayName": "moodle-core-tooltip",
+            "name": "moodle-core-tooltip",
+            "description": "Provides the base tooltip class."
+        },
+        {
+            "displayName": "moodle-course-categoryexpander",
+            "name": "moodle-course-categoryexpander",
+            "description": "Adds toggling of subcategory with automatic loading using AJAX.\n\nThis also includes application of an animation to improve user experience."
+        },
+        {
+            "displayName": "moodle-course-management",
+            "name": "moodle-course-management",
+            "description": "Provides drop down menus for list of action links."
+        },
+        {
+            "displayName": "moodle-course-util",
+            "name": "moodle-course-util",
+            "description": "The Moodle.core_course.util classes provide course-related utility functions."
+        },
+        {
+            "displayName": "moodle-course-util-cm",
+            "name": "moodle-course-util-cm",
+            "description": "A collection of utility classes for use with course modules."
+        },
+        {
+            "displayName": "moodle-course-util-section",
+            "name": "moodle-course-util-section",
+            "description": "A collection of utility classes for use with course sections."
+        },
+        {
+            "displayName": "moodle-tool_capability-search",
+            "name": "moodle-tool_capability-search",
+            "description": "This file contains the capability overview search functionality."
+        },
+        {
+            "displayName": "theme_bootstrapbase-bootstrap",
+            "name": "theme_bootstrapbase-bootstrap",
+            "description": "The Moodle Bootstrap theme's bootstrap JavaScript"
+        }
+    ]
+} };
+});
